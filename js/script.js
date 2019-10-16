fb876967bf968682d82afef4f929e86d0e6b1f59
ninoschelcher <onin1998@gmail.com>
2019-10-16 10:13:59 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,26 +5,19 @@
 
 //Openen en sluiten van categorie filter op de programma pagina//
 var categorieKnop = document.getElementById("categorie");
+var lijst = document.getElementById("criteria");
 
 function openCategorie() {
-
-    var lijst = document.getElementById("criteria");
-
-    if (lijst.classList.contains("toon")) {
-        lijst.classList.remove("toon")
-    } else {
-        lijst.classList.add("toon")
-    }
+    console.log("test");
+        lijst.classList.toggle("toon");
 }
 
 categorieKnop.addEventListener("click", openCategorie);
 
 
-
-
 //Openen van de knop "Meer" in de navigatie op desktopformaat//
-var meerKnop = document.getElementById("meer");
-var meerMenu = document.getElementById("meermenu");
+var meerKnop = document.querySelector(".meer");
+var meerMenu = document.querySelector(".meermenu");
 var body = document.querySelector("body");
 
 function openMeer() {
@@ -32,11 +25,11 @@ function openMeer() {
     body.style.overflow = ("hidden");
 }
 
-meerKnop.addEventListener("click", openMeer);
+meerKnop.addEventListener("click", openMeer, false);
 
 
 //Sluiten van het "Meer" menu op desktopformaat//
-var closeKnop = document.getElementById("close");
+var closeKnop = document.querySelector(".close");
 
 function closeMeer() {
     meerMenu.classList.toggle("aan");
@@ -48,7 +41,7 @@ closeKnop.addEventListener("click", closeMeer);
 
 
 //Hamburger navigatie op responsive formaat//
-var hamburger = document.getElementById("hamburgerknop");
+var hamburger = document.querySelector(".hamburgerknop");
 var navigatie = document.querySelector("nav");
 
 function hamburgerMenu() {
@@ -56,3 +49,10 @@ function hamburgerMenu() {
 }
 
 hamburger.addEventListener("click", hamburgerMenu);
+
+//categorie filteren
+var artiestBlokjes = document.querySelectorAll(".achtergrond");
+
+artiestBlokjes.forEach(function(artiestblok){
+    artiestblok.classList.add("hide");
+});
